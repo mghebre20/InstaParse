74de24ca342bc7332974ef9f8acf85edec355f4e
Million Ghebre <mghebre1@fb.com>
2020-07-08 10:44:52 -0700

    completed parse-dashboard setup, login activity, capturing and posting image feature, fragments using bottom navigation view

---
+++ app/src/main/java/com/example/instaparse/LoginActivity.java
@@ -0,0 +1,85 @@
+package com.example.instaparse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
+public class LoginActivity extends AppCompatActivity {
+
+    public static final String TAG = "LoginActivity";
+
+    //member variables
+    private EditText etUsername;
+    private EditText etPassword;
+    private Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        //if user is signed in already, then goToMainActivity
+        if(ParseUser.getCurrentUser() != null) {
+            goToMainActivity();
+        }
+
+        //assigning member variables to elements
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        //set a onClickListener on the button
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            Log.i(TAG, "onClick login button ");
+            String username = etUsername.getText().toString();
+            String password = etPassword.getText().toString();
+            loginUser(username, password);
+
+        }
+      });
+    }
+
+    private void loginUser(String username, String password) {
+        Log.i(TAG, "Attempting to log in user" + username);
+        //loginBackground executes logic on background thread
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                //if request fails, the ParseException wil not be null
+                if(e != null){
+                    //TODO: better error handling
+                    Log.e(TAG, "Error with login", e);
+                    Toast.makeText(LoginActivity.this, "Issue with login", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                //if request succeded, the ParseException will be null
+                //Todo: navigate to the main activity if the user has signed in properly
+                goToMainActivity();
+                //toast to indicate to the user that they have successfully logged in
+                Toast.makeText(LoginActivity.this,"Success!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void goToMainActivity() {
+        //Intent to traverse from here to MainActivity
+        //Takes in two parameters: this and our navigation point, MainActivity
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+
+        //finish the LoginActivity once navigated
+        //this will remove loginActivity from the back stack
+        finish();
+    }
+}
---
+++ app/src/main/java/com/example/instaparse/Post.java
@@ -0,0 +1,46 @@
+package com.example.instaparse;
+
+import com.parse.ParseClassName;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+
+@ParseClassName("Post")
+public class Post extends ParseObject {
+
+    //define keys from Parse
+    public static final String KEY_DESCRIPTION = "description";
+    public static final String KEY_IMAGE = "image";
+    public static final String KEY_USER = "user";
+
+    //GETTER that will return a string
+    public String getDescription() {
+        //getString is a method from the ParseObject class
+        return getString(KEY_DESCRIPTION);
+    }
+
+    //SETTER
+    public void setDescription(String description) {
+        put(KEY_DESCRIPTION, description);
+    }
+
+    //GETTER
+    public ParseFile getImage() {
+        return getParseFile(KEY_IMAGE);
+    }
+
+    //SETTER
+    public void setImage(ParseFile parseFile) {
+        put(KEY_IMAGE, parseFile);
+    }
+
+    //GETTER
+    public ParseUser getUser() {
+        return getParseUser(KEY_USER);
+    }
+
+    //SETTER
+    public void setUser(ParseUser user) {
+        put(KEY_USER, user);
+    }
+}
